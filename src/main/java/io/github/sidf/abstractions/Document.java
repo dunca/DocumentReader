1f6c69095b5a2e5c7433401f15cdf2f686012f68
sidf <flv@mail.com>
2017-03-25 17:03:28 +0200

    Document: initial release

---
+++ src/main/java/io/github/sidf/abstractions/Document.java
@@ -0,0 +1,46 @@
+package io.github.sidf.abstractions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import io.github.sidf.Bookmark;
+import io.github.sidf.exceptions.InvalidDocumentException;
+
+public abstract class Document {
+	private File file;
+	private int pageCount;
+	private Bookmark bookmark;
+	private String documentPath;
+	private String documentName;
+	
+	public Document(String filePath) throws Exception {
+		file = new File(filePath);
+		
+		if (!file.exists()) {
+			throw new FileNotFoundException(String.format("%s does not exist", filePath));
+		} else if (file.isDirectory()) {
+			throw new InvalidDocumentException(String.format("%s is not a file", filePath));
+		}
+		
+		bookmark = new Bookmark();
+		
+		documentPath = filePath;
+		documentName = file.getName();
+	}
+	
+	public int getPageCount() {
+		return pageCount;
+	}
+	
+	public String getDocumentPath() {
+		return documentPath;
+	}
+	
+	public String getDocumentName() {
+		return documentName;
+	}
+	
+	public Bookmark getBookmark() {
+		return bookmark;
+	}
+}
