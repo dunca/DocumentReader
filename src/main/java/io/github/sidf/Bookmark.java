a56ed33aa0722229c1eba3bfecd9d7f1732f2fbb
sidf <flv@mail.com>
2017-03-25 19:53:54 +0200

    Bookmark: use Page instances instead of integer page indices

--- src/main/java/io/github/sidf/Bookmark.java
+++ src/main/java/io/github/sidf/Bookmark.java
@@ -1,14 +1,27 @@
 package io.github.sidf;
 
 public class Bookmark {
-	private int pageIndex;
+	private Page page;
 	private int characterIndex;
 	
-	public int getPageIndex() {
-		return pageIndex;
+	public Bookmark(Page page, int characterIndex) {
+		this.page = page;
+		this.characterIndex = characterIndex;
+	}
+	
+	public Page getPage() {
+		return page;
 	}
 	
 	public int getCharacterIndex() {
 		return characterIndex;
+	}
+
+	public void setPage(Page page) {
+		this.page = page;
+	}
+
+	public void setCharacterIndex(int characterIndex) {
+		this.characterIndex = characterIndex;
 	}
 }
