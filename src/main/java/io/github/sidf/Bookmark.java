cbc2016ab41db77fb169db99df6a8c9fe48e9d8d
sidf <flv@mail.com>
2017-03-25 17:03:54 +0200

    Bookmark: initial release

---
+++ src/main/java/io/github/sidf/Bookmark.java
@@ -0,0 +1,14 @@
+package io.github.sidf;
+
+public class Bookmark {
+	private int pageIndex;
+	private int characterIndex;
+	
+	public int getPageIndex() {
+		return pageIndex;
+	}
+	
+	public int getCharacterIndex() {
+		return characterIndex;
+	}
+}
