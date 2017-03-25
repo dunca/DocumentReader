2415fe48110f5a7cdb22b23e6a798eda37b76d93
sidf <flv@mail.com>
2017-03-25 17:26:54 +0200

    PdfDocument: set the page count

--- src/main/java/io/github/sidf/PdfDocument.java
+++ src/main/java/io/github/sidf/PdfDocument.java
@@ -1,9 +1,16 @@
 package io.github.sidf;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 import io.github.sidf.abstractions.Document;
 
 public class PdfDocument extends Document {
+	private PDDocument pdfDocument;
+	
 	public PdfDocument(String path) throws Exception {
 		super(path);
+		
+		pdfDocument = PDDocument.load(getFile());
+		setPageCount(pdfDocument.getNumberOfPages());
 	}
 }
