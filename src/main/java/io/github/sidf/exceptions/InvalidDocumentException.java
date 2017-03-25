a58fb85311f70cab0bf1d85be3ea0e9414c13aee
sidf <flv@mail.com>
2017-03-25 17:04:10 +0200

    InvalidDocumentException: initial release

---
+++ src/main/java/io/github/sidf/exceptions/InvalidDocumentException.java
@@ -0,0 +1,11 @@
+package io.github.sidf.exceptions;
+
+public class InvalidDocumentException extends Exception {
+	public InvalidDocumentException() {
+		
+	}
+	
+	public InvalidDocumentException(String message) {
+		super(message);
+	}
+}
