1b77b913042560b814d327867cdb39a5fe89e4a9
sidf <flv@mail.com>
2017-03-25 17:03:40 +0200

    PdfDocument: initial release

---
+++ src/main/java/io/github/sidf/PdfDocument.java
@@ -0,0 +1,9 @@
+package io.github.sidf;
+
+import io.github.sidf.abstractions.Document;
+
+public class PdfDocument extends Document {
+	public PdfDocument(String path) throws Exception {
+		super(path);
+	}
+}
