0a6b21cc9cc67d42b6f37cc2dc66bca5e69011f0
sidf <flv@mail.com>
2017-03-26 15:53:49 +0300

    Device: implemented shutDown

--- src/main/java/io/github/sidf/documentreader/system/Device.java
+++ src/main/java/io/github/sidf/documentreader/system/Device.java
@@ -1,10 +1,37 @@
 package io.github.sidf.documentreader.system;
 
+import java.io.IOException;
+
 public class Device {
 	public static OperatingSystem getOperatingSystem() {
 		if (System.getProperty("os.name").contains("Windows")) {
 			return OperatingSystem.WINDOWS;
 		}
 		return OperatingSystem.LINUX;
+	}
+	
+	public static void shutDown() {
+		shutDown(3);
+	}
+	
+	public static void shutDown(int countdown) {
+		String command = null;
+		
+		switch (getOperatingSystem()) {
+		case WINDOWS:
+			command = "shutdown.exe -s -t ";
+			break;
+
+		case LINUX:
+			command = "shutdown -h ";
+			break;
+		}
+		
+		try {
+			Runtime.getRuntime().exec(command + countdown);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
