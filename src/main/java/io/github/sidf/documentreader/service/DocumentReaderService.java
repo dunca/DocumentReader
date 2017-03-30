dfec438e47f1b6bd64a43f8b008525d31e637bf0
sidf <flv@mail.com>
2017-03-30 21:44:30 +0300

    DocumentReaderService: initial release

--- src/main/java/io/github/sidf/documentreader/service/DocumentReaderService.java
+++ src/main/java/io/github/sidf/documentreader/service/DocumentReaderService.java
@@ -1,32 +1,51 @@
 package io.github.sidf.documentreader.service;
 
-import java.awt.print.Pageable;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.io.FileNotFoundException;
 
-import io.github.sidf.documentreader.document.Document;
-import io.github.sidf.documentreader.document.EspeakReader;
+import io.github.sidf.documentreader.system.Device;
 import io.github.sidf.documentreader.document.Reader;
+import io.github.sidf.documentreader.util.enums.Speed;
+import io.github.sidf.documentreader.document.Document;
+import io.github.sidf.documentreader.util.enums.Language;
+import io.github.sidf.documentreader.document.ReaderFactory;
+import io.github.sidf.documentreader.document.DocumentLibrary;
 
-public class DocumentReaderService {
+public class DocumentReaderService implements Runnable {
 	private static Reader reader;
 	private static Document document;
-	private static String documentId;
-
 	private static DocumentReaderService instance;
+	private static boolean featureDetectionEnabled;
+	private static DocumentLibrary documentLibrary;
 	
-	private DocumentReaderService() {
-		
+	
+	private DocumentReaderService(File libraryPath) throws FileNotFoundException {
+		documentLibrary = new DocumentLibrary(libraryPath);
 	}
 	
-	public DocumentReaderService getInstance() {
+	public DocumentReaderService getInstance(File libraryPath) throws FileNotFoundException {
 		if (instance == null) {
-			instance = new DocumentReaderService();
+			instance = new DocumentReaderService(libraryPath);
+		} else if (documentLibrary.getLibraryPath() != libraryPath) {
+			throw new RuntimeException("Cannot re-instantiate the class with a new library path");
 		}
 		
 		return instance;
 	}
 	
-	public void setDocumentId(String documentId) {
-		this.documentId = documentId;
+	public void setDocument(String documentId) throws IOException {
+		document = documentLibrary.getDocumentById(documentId);
+	}
+	
+	public Map<String, String> getDocumentNameMap() {
+		return documentLibrary.getDocumentNameMap();
+	}
+	
+	public void setReader(String readerName) throws Exception {
+		reader = ReaderFactory.getInstance(readerName, document.getBookmark().getPage());
 	}
 	
 	public void startReading() {
@@ -37,27 +56,60 @@ public class DocumentReaderService {
 		
 	}
 	
-	public void setVolume(int level) {
-		
+	public void setAudioVolume(int level) throws Exception {
+		Device.setVolume(level);
 	}
 	
-	public int getVolume() {
-		return 1;
+	public int getAudioVolume() throws Exception {
+		return Device.getVolume();
 	}
 	
-	public void setReaderSpeed(int level) {
-		
+	public void setReaderSpeed(Speed speed) throws IOException {
+		reader.setSpeed(speed);
 	}
 	
-	public int getReaderSpeed() {
-		return 1;
+	public Speed getReaderSpeed() {
+		return reader.getSpeed();
 	}
 	
-	public void setReaderLanguage(String language) {
-		
+	public void setReaderLanguage(Language language) throws IOException {
+		reader.setLanguage(language);
 	}
 	
-	public String getReaderLanguage() {
-		return "";
+	public Language getReaderLanguage() {
+		return reader.getLanguage();
+	}
+	
+	public void setFeatureDetection(boolean enabled) {
+		featureDetectionEnabled = enabled;
+	}
+	
+	public boolean getFeatureDetection() {
+		return featureDetectionEnabled;
+	}
+	
+	public List<String> getReaderProviders() {
+		return ReaderFactory.getReaderProviders();
+	}
+
+	public Language[] getSupportedLanguages() {
+		return reader.getSupportedLanguages();
+	}
+	
+	public Speed[] getSupportedSpeed() {
+		return reader.getSupportedSpeed();
+	}
+
+	public void updateLibrary() {
+		documentLibrary.update();
+	}
+	
+	public void shutDown() throws IOException {
+		Device.shutDown();
+	}
+	
+	@Override
+	public void run() {
+		startReading();
 	}
 }
