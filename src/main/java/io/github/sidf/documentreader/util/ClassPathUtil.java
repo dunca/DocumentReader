d003900c788bfe7c5eeb2a833b24571f77311738
sidf <flv@mail.com>
2017-03-30 14:47:34 +0300

    ClassPathUtil: initial release

---
+++ src/main/java/io/github/sidf/documentreader/util/ClassPathUtil.java
@@ -0,0 +1,12 @@
+package io.github.sidf.documentreader.util;
+
+import java.util.List;
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;;
+
+public class ClassPathUtil {
+	// http://stackoverflow.com/questions/7317179/scan-the-classpath-for-classes-with-custom-annotation
+	public static List<String> getSubclassNames(String packageName, Class abstractClass) {
+		List<String> classNames = new FastClasspathScanner(packageName).scan().getNamesOfSubclassesOf(abstractClass);
+		return classNames;
+	}
+}
