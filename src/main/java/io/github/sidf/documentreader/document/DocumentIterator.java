aa069edcafef25e236ef5eee9f8d1a5b5350e982
sidf <flv@mail.com>
2017-03-31 15:30:59 +0300

    DocumentIterator: fixed the hasNext method

--- src/main/java/io/github/sidf/documentreader/document/DocumentIterator.java
+++ src/main/java/io/github/sidf/documentreader/document/DocumentIterator.java
@@ -11,7 +11,7 @@ public class DocumentIterator implements Iterator<DocumentPage> {
 	}
 	
 	public boolean hasNext() {
-		return document.getBookmark().getPageIndex() <= document.getPageCount() - 1;
+		return document.getBookmark().getPageIndex() < document.getPageCount() - 1;
 	}
 
 	public DocumentPage next() {
