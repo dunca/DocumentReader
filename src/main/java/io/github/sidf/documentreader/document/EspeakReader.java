1444c2cc36f9daca1c0caac31d2c722f26bb97be
sidf <flv@mail.com>
2017-03-31 14:55:18 +0300

    EspeakReader: removed useless Windows support

--- src/main/java/io/github/sidf/documentreader/document/EspeakReader.java
+++ src/main/java/io/github/sidf/documentreader/document/EspeakReader.java
@@ -1,30 +1,19 @@
 package io.github.sidf.documentreader.document;
 
-import java.io.File;
 import java.util.Map;
 import java.util.HashMap;
-import java.io.IOException;
 
-import io.github.sidf.documentreader.system.Device;
 import io.github.sidf.documentreader.util.enums.Speed;
 import io.github.sidf.documentreader.util.enums.Language;
-import io.github.sidf.documentreader.system.enums.OperatingSystem;
 
 public class EspeakReader extends Reader {
-	private String espeakPath;
+	private static final String shellPath = "/bin/sh";
 	private Map<Speed, String> speedMap = new HashMap<>();
 	private Map<Language, String> languageMap = new HashMap<>();
+	private static final String espeakCommandTemplate =  "/usr/bin/espeak -s %s -v %s \"%s\" --stdout | aplay";
 	
 	public EspeakReader(Document document) throws Exception {
 		super(document);
-		
-		if (Device.getOperatingSystem() == OperatingSystem.LINUX) {
-			espeakPath = "/usr/bin/espeak";
-		} else {
-			String programFilesPath = System.getenv("ProgramFiles");
-			String programFiles86Path = programFilesPath += " (x86)";
-			espeakPath =  new File(programFiles86Path).exists() ? programFiles86Path + "/eSpeak/command_line/espeak.exe" : programFilesPath + "/eSpeak/command_line/espeak.exe";
-		}
 		
 		speedMap.put(Speed.FAST, "200");
 		speedMap.put(Speed.MEDIUM, "150");
@@ -38,9 +27,9 @@ public class EspeakReader extends Reader {
 	}
 
 	@Override
-	// todo only add the shell if running on linux; run espeak's output though aplay to avoid rpi lag
 	public void read(String text) throws Exception {
-		Process process = Runtime.getRuntime().exec(new String[] {"/bin/sh", "-c", String.format("%s -s %s -v %s \"%s\"", espeakPath, speedMap.get(getSpeed()), languageMap.get(getLanguage()), text)});
+		String espeakCommand = String.format(espeakCommandTemplate, speedMap.get(getSpeed()), languageMap.get(getLanguage()), text);
+		Process process = Runtime.getRuntime().exec(new String[] {shellPath, "-c", espeakCommand});
 		process.waitFor();
 	}
 
