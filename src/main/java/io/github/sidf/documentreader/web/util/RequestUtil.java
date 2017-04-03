e5d6d265d43f3a5c29a54589237caeb6b7944996
sidf <flv@mail.com>
2017-04-03 12:20:20 +0300

    RequestUtil: properly handle unicode

--- src/main/java/io/github/sidf/documentreader/web/util/RequestUtil.java
+++ src/main/java/io/github/sidf/documentreader/web/util/RequestUtil.java
@@ -1,7 +1,9 @@
 package io.github.sidf.documentreader.web.util;
 
+import java.net.URLDecoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.UnsupportedEncodingException;
 
 public class RequestUtil {
 	private RequestUtil() {
@@ -9,7 +11,13 @@ public class RequestUtil {
 	}
 	
 	public static String parseBodyString(String body, String elementName) {
-		Pattern pattern = Pattern.compile(String.format("(?<=%s\\=)[\\w\\.]+(?=(&|$))", elementName));
+		try {
+			body = URLDecoder.decode(body, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// do nothing about it
+		}
+		
+		Pattern pattern = Pattern.compile(String.format("(?<=%s\\=)[\\w\\.]+(?=(&|$))", elementName), Pattern.UNICODE_CHARACTER_CLASS);
 		Matcher matcher = pattern.matcher(body);
 		if (matcher.find()) {
 			return matcher.group();
