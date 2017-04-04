0d91546d98ca1e443d5c370b5064cc0a156803a5
sidf <flv@mail.com>
2017-04-04 23:23:35 +0300

    WebInterface: allow access to the log file via an url

--- src/main/java/io/github/sidf/documentreader/web/WebInterface.java
+++ src/main/java/io/github/sidf/documentreader/web/WebInterface.java
@@ -1,5 +1,7 @@
 package io.github.sidf.documentreader.web;
 
+import spark.Request;
+import spark.Response;
 import spark.Route;
 import spark.Spark;
 import java.io.File;
@@ -8,17 +10,20 @@ import java.io.IOException;
 import org.ini4j.InvalidFileFormatException;
 
 import io.github.sidf.documentreader.service.DocumentReaderService;
+import io.github.sidf.documentreader.util.FileUtil;
 
 public class WebInterface {
 	private int port;
+	private String logPath;
 	private String configPath;
 	private String libraryPath;
 	private static DocumentReaderService service;
 	
-	public WebInterface(String libraryPath, String configPath, DocumentReaderService documentReaderService) throws InvalidFileFormatException, IOException {
+	public WebInterface(String libraryPath, String configPath, String logPath, DocumentReaderService documentReaderService) throws InvalidFileFormatException, IOException {
 		Ini ini = new Ini(new File(configPath));
 		this.port = Integer.valueOf(ini.get("Web UI", "port"));
 		
+		this.logPath = logPath;
 		this.configPath = configPath;
 		this.libraryPath = libraryPath;
 		service = documentReaderService;
@@ -32,6 +37,12 @@ public class WebInterface {
 		
 		Spark.get("/", route);
 		Spark.post("/", route);
+		Spark.get("/log", new Route() {
+			@Override
+			public Object handle(Request request, Response response) throws Exception {
+				return FileUtil.fileToString(new File(logPath));
+			}
+		});
 	}
 	
 	public void stop() {
