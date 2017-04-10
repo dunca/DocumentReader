b782cc5117dc9f1b7a7f55bc8157ffb1c240c665
sidf <flv@mail.com>
2017-04-10 21:48:53 +0300

    Page: removed unused length field

--- src/main/java/io/github/sidf/documentreader/document/Page.java
+++ src/main/java/io/github/sidf/documentreader/document/Page.java
@@ -3,13 +3,11 @@ package io.github.sidf.documentreader.document;
 import java.util.Iterator;
 
 public class Page implements Iterable<String> {
-	private int length;
 	private String content;
 	private Bookmark sourceBookmark;
 	
 	Page(Bookmark sourceBookmark, String content) {
 		this.sourceBookmark = sourceBookmark;
-		this.length = content.length();
 		this.content = content;
 	}
 		
