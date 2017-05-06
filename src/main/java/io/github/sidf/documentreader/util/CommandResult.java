901f5ed8cde5556460efb533d6ad6af5b9439ec6
sidf <flv@mail.com>
2017-05-06 23:26:14 +0300

    CommandResult: added comments

--- src/main/java/io/github/sidf/documentreader/util/CommandResult.java
+++ src/main/java/io/github/sidf/documentreader/util/CommandResult.java
@@ -1,5 +1,9 @@
 package io.github.sidf.documentreader.util;
 
+/**
+ * Represents the information resulted from a command's execution 
+ * @author Esc
+ */
 public class CommandResult {
 	private String stdout;
 	private int exitValue;
@@ -9,10 +13,19 @@ public class CommandResult {
 		this.exitValue = exitValue;
 	}
 
+	/**
+	 * Gets the standard output
+	 * @return a string denoting the standard output of the command. It's what usually shows up in the
+	 * terminal (the default standard output)
+	 */
 	public String getStdout() {
 		return stdout;
 	}
 
+	/**
+	 * Gets the exit value
+	 * @return an integer denoting the commands exit code. A value of 0 indicates normal process termination by convention
+	 */
 	public int getExitValue() {
 		return exitValue;
 	}
