b009b09e85280507946708100949b83a3d3944bd
sidf <flv@mail.com>
2017-05-06 19:58:43 +0300

    Device: added comments

--- src/main/java/io/github/sidf/documentreader/system/Device.java
+++ src/main/java/io/github/sidf/documentreader/system/Device.java
@@ -15,8 +15,19 @@ import io.github.sidf.documentreader.util.CommandResult;
 public class Device {
 	private static Logger logger = Logger.getLogger(Device.class.getName());
 	
+	/**
+	 * Command that is used to check if a program is installed on the system
+	 */
 	private static final String debianPackageCheckTemplate = "dpkg -s %s";
+	
+	/**
+	 * Regex pattern that is used to extract the numeric audio volume level
+	 */
 	private static final Pattern volumePattern = Pattern.compile("(?<=\\[)\\d+(?=%\\])");
+	
+	/**
+	 * List of program packages that contain programs which are necessary for the application to function
+	 */
 	private static String[] dependecies = new String[] { "espeak", "hostapd", "dnsmasq", "libopencv2.4-jni", "poppler-utils",
 														 "psmisc", "grep", "iproute2", "uvcdynctrl", "procps", "alsa-utils"};
 	
@@ -24,26 +35,59 @@ public class Device {
 		
 	}
 	
+	/**
+	 * Checks if the device fulfills the requirements necessary for the application to function
+	 * @return 'true' if all of the following methods return 'true'
+	 * <ul>
+	 * <li>the operating system is supported</li>
+	 * <li>the applications was ran with root permissions</li>
+	 * <li>all the software dependencies are satisfied</li>
+	 * </ul>
+	 */
 	public static boolean meetsRequirements() {	
 		return osSupported() && runsAsRoot() && dependenciesSatisfied();
 	}
 	
+	/**
+	 * Reboots or shuts down the device, depending on the value of the parameter
+	 * @param reboot if 'true', the device will be rebooted
+	 * @throws IOException if an I/O exception occurs while running the system command
+	 */
 	private static void togglePowerState(boolean reboot) throws IOException {
 		Runtime.getRuntime().exec(reboot ? "reboot" : "poweroff");
 	}
 	
+	
+	/**
+	 * Shuts down the device
+	 * @throws IOException if an I/O error occurs
+	 */
 	public static void shutDown() throws IOException {
 		togglePowerState(false);
 	}
 	
+	/**
+	 * Reboots the device
+	 * @throws IOException if an I/O error occurs
+	 */
 	public static void reboot() throws IOException {
 		togglePowerState(true);
 	}
 	
+	/**
+	 * Sets the audio volume level
+	 * @param level a integer between 0 and 100, denoting the volume level
+	 * @throws Exception if the volume cannot be set due to various reasons (amixer is not installed)
+	 */
 	public static void setVolume(int level) throws Exception {
 		CommandUtil.executeCommand(String.format("amixer sset PCM %d%%", level));
 	}
 	
+	/**
+	 * Gets the audio volume level
+	 * @return an integer between 0 and 100 denoting the current volume level
+	 * @throws Exception if the output provided by amixer cannot be parsed or if an I/O error occurs
+	 */
 	public static Integer getVolume() throws Exception {
 		CommandResult commandResult = CommandUtil.executeCommand("amixer get PCM | tail -1");
 		Matcher matcher = volumePattern.matcher(commandResult.getStdout());
@@ -55,6 +99,10 @@ public class Device {
 		return Integer.valueOf(matcher.group());
 	}
 	
+	/**
+	 * Checks if all the programs that this application relies on in order to function are installed
+	 * @return 'true' if all the required programs seem to be installed
+	 */
 	private static boolean dependenciesSatisfied() {
 		List<String> unsatisfiedDependencies = new ArrayList<>();
 		
@@ -82,6 +130,9 @@ public class Device {
 		return true;
 	}
 	
+	/**
+	 * @return 'true' if the application seems to be running with root permissions
+	 */
 	private static boolean runsAsRoot() {
 		CommandResult commandResult = null;
 		
@@ -95,6 +146,10 @@ public class Device {
 		return commandResult.getStdout().equals("root");
 	}
 	
+	/**
+	 * @return 'true' if the operating system running on the device is supported  by the
+	 * application. (Debian or Debian derivatives)
+	 */
 	private static boolean osSupported() {
 		return new File("/etc/debian_version").exists();
 	}
