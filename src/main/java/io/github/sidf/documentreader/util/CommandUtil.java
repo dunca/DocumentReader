c53b8272d9396d5e894c73563c46cbdda50f030f
sidf <flv@mail.com>
2017-05-06 23:06:11 +0300

    CommandUtil: added comments

--- src/main/java/io/github/sidf/documentreader/util/CommandUtil.java
+++ src/main/java/io/github/sidf/documentreader/util/CommandUtil.java
@@ -1,9 +1,12 @@
 package io.github.sidf.documentreader.util;
 
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A set of utility methods that deal with commands and processes
+ * @author Esc
+ */
 public class CommandUtil {
 	private static Logger logger = Logger.getLogger(CommandUtil.class.getName());
 	
@@ -11,12 +14,21 @@ public class CommandUtil {
 		
 	}
 	
-	public static CommandResult executeCommand(String command) throws IOException, InterruptedException {
+	/**
+	 * Executes a system command
+	 * @param command the command to run, with space separated parameters (if any)
+	 * <p>
+	 * Special treatment for commands that rely on a shell in order to run correctly. The pipe "|" is a 
+	 * shell feature. We also have to use a shell to run commands that contain quotes, especially if 
+	 * there are spaces inside the quotes (http://stackoverflow.com/a/31776547)
+	 * @return a CommandResult instance containing information about the command that 
+	 * either finished running or became a daemon
+	 * @throws Exception if an I/O error occurs (command not found, etc.) or if the current thread is interrupted while
+	 * waiting for the process to finish its execution
+	 */
+	public static CommandResult executeCommand(String command) throws Exception {
 		String[] commandArray = null;
 		
-		// special treatment for commands that rely on a shell in order to run correctly. The pipe "|" is a 
-		// shell feature. We also have to use a shell to run commands that contain quotes, especially if 
-		// there are spaces inside the quotes (http://stackoverflow.com/a/31776547)
 		if (command.contains("|") || command.contains("'") || command.contains("\"")) {
 			commandArray = new String[] {"/bin/sh", "-c", command};
 		} else {
@@ -24,6 +36,8 @@ public class CommandUtil {
 		}
 		
 		ProcessBuilder processBuilder = new ProcessBuilder(commandArray);
+		
+		// will redirect stderr to stdout so we can capture both using the same input stream
 		processBuilder.redirectErrorStream(true);
 		
 		logger.info(String.format("Trying to run command %s", command));
@@ -33,6 +47,11 @@ public class CommandUtil {
 		return new CommandResult(StreamUtil.inputStreamToString(process.getInputStream()), exitCode);
 	}
 	
+	/**
+	 * Terminates a system process
+	 * @param processName the name of the process
+	 * @throws Exception if an error occurs while trying to terminate the process (killall is not installed, etc.)
+	 */
 	public static void terminateProcess(String processName) throws Exception {
 		if (!processRunning(processName)) {
 			logger.info(String.format("Can't terminate %s. It isn't running", processName));
@@ -45,6 +64,11 @@ public class CommandUtil {
 		}
 	}
 	
+	/**
+	 * Checks if a process is running
+	 * @param processName the name of the process
+	 * @return 'true' if a process with the given name is running
+	 */
 	public static boolean processRunning(String processName) {
 		return new ValidatableCommand(String.format("pgrep %s", processName)).runs();
 	}
